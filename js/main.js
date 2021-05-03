ff6fd62c56f51532a991a62e7d34b5de792f0b0d
bxphxmxth <fushs@hotmail.fr>
2021-05-03 10:14:20 +0200

    exomethod6

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// ### 1. let nombre = 8.56
+
+let nombre = 8.56
+
+// ### 2.  Arrondir au nombre à l'entier le plus proche
+
+console.log(Math.round(nombre));
+
+// ### 3.  Arrondir à l'entier vers le haut
+
+console.log(Math.ceil(nombre));
+// ### 4.  Arrondir à l'entier vers le bas
+
+console.log(Math.floor(nombre));
+
+// ### 5.  Enlever toute la partie décimale
+
+console.log(Math.trunc(nombre));
+
+// ### 6.  Afficher un nombre aléatoire entre 0-1
+
+let random = Math.round(Math.random());
+console.log(random);
+
+// ### 7.  Afficher un nombre aléatoire entre 0-100
+
+let random2 = Math.random()*100;
+console.log(random2);
+
+// ### 8.  Afficher un nombre entier aléatoire entre 0-100
+
+let random3 = Math.round(Math.random()*100);
+console.log(random3);
+
+// ### 9.  Afficher 8 puissance 2
+
+console.log(Math.pow(8,2));
+
+// ### 10.  Afficher racine carré de 9
+
+
+console.log(Math.sqrt(9));
+
+// ### 11.  Afficher la valeur absolue de -1
+
+console.log(Math.abs(-1));
+
+// ### 12.  Afficher la valeur la plus grande parmi -2, 1000, 8, 57
+
+console.log(-2,1000,8,57);
+console.log(Math.max(-2,1000,8,57));
+
+// ### 13.  Afficher la valeur la plus petite parmi -2, 1000, 8, 57
+
+console.log(-2,1000,8,57);
+console.log(Math.min(-2,1000,8,57));
